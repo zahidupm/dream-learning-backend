3d8d9750590f23ac76cba053704923faf7c8bb1c
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-25 22:36:14 +0600

    learners and index file added

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(cors());
 const Port = process.env.Port || 5000;
+
+const items = require('./data/learners.json')
 
 app.get('/', (req, res) => {
     res.send('Server is Running!')
+})
+
+app.get('/all', (req, res) => {
+    res.send(items)
+})
+
+app.get('/item/:id', (req, res) => {
+    const id = req.params.id;
+    const singleItem = items?.find(item => item.id == id);
+    if(!singleItem) {
+        res.send('no item found')
+    }
+    res.send(singleItem);
 })
 
 app.listen(Port, () => {
